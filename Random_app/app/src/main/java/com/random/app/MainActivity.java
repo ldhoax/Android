9e7c28e2e1ad56e7ced716512d1a4a0d87ef8700
Lê Đức Hòa <leduchoabatr12@gmail.com>
2019-07-20 00:02:19 +0700

    add Show announce when the second number not greater than the first one

--- Random_app/app/src/main/java/com/random/app/MainActivity.java
+++ Random_app/app/src/main/java/com/random/app/MainActivity.java
@@ -14,40 +14,48 @@ import org.w3c.dom.Text;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
     EditText number1,number2;
     TextView number;
     Button click_to_random;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // ánh xạ
         number = (TextView)findViewById(R.id.TextView_number);
         number1 = (EditText)findViewById(R.id.EditText_number1);
         number2 = (EditText) findViewById(R.id.EditText_number2);
         click_to_random = (Button)findViewById(R.id.Button_random);
+
         click_to_random.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 int num1,num2;
+                // lấy 2 chuỗi trong các edittext
                 String numstr1 = number1.getText().toString().trim();
 
                 String numstr2 = number2.getText().toString().trim();
 
-                if(numstr1.length()>0 && numstr2.length()>0)
+                if(numstr1.length()>0 && numstr2.length()>0)// kiểm tra đã nhập chưa
                 {
+                    // chuyển chuỗi thành int
                     num2 = Integer.parseInt(numstr2);
                     num1 = Integer.parseInt(numstr1);
-                    if(num2>=num1)
+
+                    if(num2>=num1)// số thứ hai phải lớn hơn số thứ nhất
                     {
                         int temp;
                         Random random = new Random();
+                        //thực hiện random
                         temp= random.nextInt(num2 - num1 +1 ) + num1;
                         number.setText(temp+"");
 
                     }
                     else
                     {
-                        Toast.makeText(MainActivity.this, "", Toast.LENGTH_LONG).show();
+                        Toast.makeText(MainActivity.this, "The second number have to greater than the first one", Toast.LENGTH_LONG).show();
                     }
                 }
                 else
